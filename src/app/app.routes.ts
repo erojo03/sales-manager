4647e1f400d35c438c1082af76d9d79b8fc3be69
erojo03 <edgar.rojo03@outlook.com>
2025-06-23 00:27:28 -0500

    feat(routing): add routes for dashboard and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'Login',
+    path: 'login',
+    loadComponent: () =>
+      import('./features/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    title: 'App',
+    path: '',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+    loadChildren: () => import('./features/dashboard/dashboard.routes'),
+  },
+];
