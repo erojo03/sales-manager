a2bf43719998f84e5fa3cc6180685b9bdb591271
erojo03 <edgar.rojo03@outlook.com>
2025-09-11 18:53:26 -0500

    ✨ feat(login): integrates the `Login Header` and `Login Feature` components into the main `Login` component.

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { LoginHeaderComponent } from './components/login-header.component';
+import { LoginFeaturesComponent } from './components/login-features.component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  template: ``,
+  imports: [LoginHeaderComponent, LoginFeaturesComponent],
+  template: `
+    <section class="space-y-8">
+      <app-login-header />
+      <app-login-features />
+    </section>
+  `,
+  host: { class: 'flex items-center gap-16' },
 })
 export class LoginComponent {}
