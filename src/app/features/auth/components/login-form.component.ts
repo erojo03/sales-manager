5de4340ef9725bab89e4a6e48ba1d2bfdf7c5a19
erojo03 <edgar.rojo03@outlook.com>
2025-09-16 17:46:03 -0500

    feat(auth): create login and register form components

---
+++ src/app/features/auth/components/login-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-form',
+  imports: [],
+  template: `<h1>Login Form</h1>`,
+})
+export class LoginFormComponent {}
---
+++ src/app/features/auth/components/register-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form',
+  imports: [],
+  template: `<h1>Register Form</h1>`,
+})
+export class RegisterFormComponent {}
