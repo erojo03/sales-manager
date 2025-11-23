714f5c01eca1c56e2bd0edb0f027507da81b729e
erojo03 <edgar.rojo03@outlook.com>
2025-11-22 21:42:04 -0500

    feat(auth): add router outlet for authentication views

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { AuthHeaderComponent } from './components/auth-header.component';
 import { AuthFeaturesComponent } from './components/auth-features.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
-  imports: [AuthHeaderComponent, AuthFeaturesComponent],
+  imports: [AuthHeaderComponent, AuthFeaturesComponent, RouterOutlet],
   template: `
-    <section class="hidden space-y-8 lg:block">
+    <section class="hidden flex-1 space-y-8 lg:block">
       <app-auth-header />
       <app-auth-features />
     </section>
+
+    <section
+      class="flex w-full flex-col rounded-lg border-0 bg-white/80 p-6 text-gray-900 shadow-2xl backdrop-blur-sm sm:w-md lg:w-sm xl:w-md">
+      <router-outlet />
+    </section>
   `,
   host: {
-    class: 'flex items-center justify-center p-2.5 gap-15 lg:p-5 container',
+    class: 'flex items-center justify-center gap-15 container',
   },
 })
 export class AuthComponent {}
